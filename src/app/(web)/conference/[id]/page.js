02ca92d59698d5394567c679a57cb86029d696a7
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-03-11 13:58:09 +0600

    conference static page design completed

--- src/app/(web)/conference/[id]/page.js
+++ src/app/(web)/conference/[id]/page.js
@@ -1,10 +1,36 @@
+import Image from "next/image";
+import organizerImg from '../../../../assets/organizer-1.png'
+import doubleArrow from '../../../../assets/double-arrow.svg'
 
-
-const ConferencePage = () => {
+const ConferencePage = async() => {
   return (
-    <div>
-      this is conference page
-    </div>
+    <main className="px-6 py-[100px]">
+      <div className="w-full max-w-[1320px]">
+        <h2 className="text-2xl md:text-5xl font-bold mb-6">Conference name goes here</h2>
+        <p className="text-custom-gray">Lorem uis diam turpis quam id fermentum.In quis diam turpis quam id fermentum.</p>
+      </div>
+      <div className="mt-16 grid grid-cols-12 gap-x-[50px]">
+        <div className="col-span-4">
+          <div className="bg-white hover:bg-primary flex items-center gap-x-[45px] p-2 border rounded-lg cursor-pointer hover:shadow-[rgba(17,_17,_26,_0.1)_0px_4px_16px,_rgba(17,_17,_26,_0.05)_0px_8px_32px] transition-all ease-out duration-100">
+            <div className="h-14 w-14 rounded-lg flex justify-center items-center bg-white">
+              <Image src={doubleArrow} height={22} width={22} alt="Organizer" />
+            </div>
+            <p className="text-[20px] font-bold">Organizer</p>
+          </div>
+        </div>
+        <div className="col-span-8 bg-off-white py-[68px] px-[66px] rounded-lg">
+          <div className="bg-white rounded-lg p-4 w-full flex gap-10 items-center">
+            <div className="w-[140px] h-[140px]">
+              <Image src={organizerImg}  objectFit="cover" alt="Organizer" />
+            </div>
+            <div>
+              <p className="font-bold text-[20px] mb-3">Siddhartha</p>
+              <p className="text-custom-gray">Lorem ipsum dolor sit amet, con sec tetur ad.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
--- src/components/home/SponsorSection.jsx
+++ src/components/home/SponsorSection.jsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 const SponsorSection = async () => {
   const res = await fetch("http://localhost:5000/sponsors");
   const sponsorData = await res.json();
@@ -20,14 +18,62 @@ const SponsorSection = async () => {
                     alt="Sponsor"
                     className="h-[19px] w-auto mx-auto my-[40px]"
                   />
-                  <div className="flex flex-wrap items-center justify-center gap-x-[55px] space-y-8 lg:space-y-0">
-                    {sponsor?.sponsorCategory?.map((item) => (
-                      <div key={item?.id} className="">
-                        <img
-                          src={item?.logo}
-                          alt="Sponsor Logo"
-                          className="w-auto h-auto"
-                        />
+                  <div
+                    className={`grid grid-cols-12 items-center justify-center space-y-8 lg:space-y-0 ${
+                      sponsor.id === 2 ? "gap-x-0" : "gap-x-10"
+                    }`}
+                  >
+                    {sponsor?.goldenSponsors?.map((item) => (
+                      <div key={item?.id} className="col-span-6">
+                        <div
+                          className={`flex ${
+                            item?.id === 1 ? "justify-start md:justify-end" : "justify-start"
+                          }`}
+                        >
+                          <img
+                            src={item?.logo}
+                            alt="Sponsor Logo"
+                            className="w-auto h-auto"
+                          />
+                        </div>
+                      </div>
+                    ))}
+                    {sponsor?.silverSponsors?.map((item) => (
+                      <div key={item?.id} className="col-span-6 lg:col-span-4">
+                        <div
+                          className={`flex ${
+                            item?.id === 1
+                              ? "justify-end"
+                              : item?.id === 2
+                              ? "justify-center"
+                              : "justify-end lg:justify-start"
+                          }`}
+                        >
+                          <img
+                            src={item?.logo}
+                            alt="Sponsor Logo"
+                            className="w-auto h-auto"
+                          />
+                        </div>
+                      </div>
+                    ))}
+                    {sponsor?.bronzeSponsors?.map((item) => (
+                      <div key={item?.id} className="col-span-6 lg:col-span-3">
+                        <div
+                          className={`flex ${
+                            item?.id === 1
+                              ? "justify-end"
+                              : item?.id === 2 || item?.id === 3
+                              ? "justify-center"
+                              : "justify-start"
+                          }`}
+                        >
+                          <img
+                            src={item?.logo}
+                            alt="Sponsor Logo"
+                            className="w-auto h-auto"
+                          />
+                        </div>
                       </div>
                     ))}
                   </div>
